2bea86cccbdb0a7364280fe51d38e39f3f2a12ee
Your Name <you@example.com>
2026-09-02 15:11:33 +0530

    Added CalculatorApp program

---
+++ CalculatorApp.java
@@ -0,0 +1,10 @@
+public class CalculatorApp {
+    public static void main(String[] args) {
+        int firstNumber = 35;
+        int secondNumber = 25;
+        int total = firstNumber + secondNumber;
+
+        System.out.println("CI/CD Calculator Application");
+        System.out.println("Sum = " + total);
+    }
+}
