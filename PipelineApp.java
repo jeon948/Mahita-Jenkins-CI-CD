eced59505f2e736035dab92145950fc39d319285
Your Name <you@example.com>
2026-09-02 14:52:04 +0530

    Initial CI CD project

---
+++ PipelineApp.java
@@ -0,0 +1,6 @@
+public class PipelineApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mahita Jenkins CI/CD Pipeline!");
+        System.out.println("Application executed successfully.");
+    }
+}
